04976eb8c5f605e5026f0b58ded52d0e84a2dca3
georgecatalin <george.calin@gmail.com>
2020-04-24 16:46:07 +0300

    Variables in C++

---
+++ Cpp Essential Training/variables.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int i=7;
+    printf("The value of i is %d\n",i);
+
+    const int c=20; //const qualifier
+    printf("The value of c is %d\n",c);
+
+    return 0;
+}
