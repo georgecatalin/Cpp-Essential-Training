ef47c7490db55ef76f114ffc206b84cbbb4381fa
georgecatalin <george.calin@gmail.com>
2020-04-27 21:29:52 +0300

    Ternary condition operator.

---
+++ Cpp Essential Training/logical_operators.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    bool a=true;
+    bool b=false;
+
+    if (a || b)
+    {
+        puts("true");
+    }
+    else
+    {
+        puts("false");
+    }
+    
+    return 0;
+    
+}
