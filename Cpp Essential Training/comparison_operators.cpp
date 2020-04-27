68f2c13444f304fc159e13d4fa3e52da23b468c4
georgecatalin <george.calin@gmail.com>
2020-04-27 21:16:13 +0300

    comparison operators.

---
+++ Cpp Essential Training/comparison_operators.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int x=7;
+    int y=42;
+
+    if (x<y)
+    {
+        puts("true");
+    }
+    else
+    {
+        puts("false");
+    }
+    
+    return 0;
+}
