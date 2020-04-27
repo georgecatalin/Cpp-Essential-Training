e28fcf96cbf7145f1ac8147eef7c8a65a4fec33a
georgecatalin <george.calin@gmail.com>
2020-04-27 10:59:27 +0300

    The void type in C++

---
+++ Cpp Essential Training/the_void_type.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+void function()
+{
+    puts("This function takes no arguments and does not return anything function() ");
+}
+
+int main()
+{
+    function();
+    return 0;
+}
